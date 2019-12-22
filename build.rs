b015bee3255eec068c64e27f67ee4751c8af6e6c
Coding-Badly <arduino.tiny@gmail.com>
2019-12-22 14:24:44 -0600

    Try binding with a binary resource file.

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
-extern crate embed_resource;
+//extern crate embed_resource;
 
 fn main() {
-    embed_resource::compile("./src/version.rc");
+//    embed_resource::compile("./src/version.rc");
+    println!("cargo:rustc-link-search=native={}", "C:/ProjectsSplit/Make/G Code Insert Pause/tmp" /*out_dir*/);
+    println!("cargo:rustc-link-lib=dylib={}", "version" /*prefix*/);
 }
