edf7babed7ee9e25b522754bdedf766a2e86c475
Brian - REMOS <bcook@rowdydog.com>
2019-12-30 23:43:41 -0600

    Remove version stuff.

--- build.rs
+++
@@ -1,7 +0,0 @@
-//extern crate embed_resource;
-
-fn main() {
-//    embed_resource::compile("./src/version.rc");
-    println!("cargo:rustc-link-search=native={}", "C:/ProjectsSplit/Make/G Code Insert Pause/tmp" /*out_dir*/);
-    println!("cargo:rustc-link-lib=dylib={}", "version" /*prefix*/);
-}
